0c810260391fd3497186eebd32a8f53a9699fa9d
catlair <catlair@qq.com>
2021-05-02 13:23:35 +0800

    退回之前的模式

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -8,7 +8,7 @@ import { resolve } from 'path';
 export function getVersion() {
   let CURRENT_VERSION = 'x.x.x';
   try {
-    CURRENT_VERSION = require(resolve(__dirname, '../../config/level.json'));
+    CURRENT_VERSION = require(resolve(__dirname, '../config/level.json'));
   } catch (error) {}
   return CURRENT_VERSION;
 }

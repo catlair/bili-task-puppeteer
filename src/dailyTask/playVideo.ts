ad421d8199a42f97709fa999ddc28d60456c9f0f
catlair <catlair@qq.com>
2021-06-09 12:03:51 +0800

    增加判断视频是否已经在播放

--- src/dailyTask/playVideo.ts
+++ src/dailyTask/playVideo.ts
@@ -6,6 +6,18 @@ export default async function playVideo(page: Page, logger?: Logger) {
     logger = getLogger('playVideo');
   }
   try {
+    const isVideoPlaying = await page.$eval(
+      'video',
+      (video: HTMLVideoElement) =>
+        new Promise(res => {
+          const prevTime = video.currentTime;
+          setTimeout(() => res(prevTime !== video.currentTime), 1000);
+        }),
+    );
+    if (isVideoPlaying) {
+      logger.debug('视频正在播放...');
+      return;
+    }
     logger.debug('播放视频...');
     const $video = await page.$('video');
     await $video.click();
